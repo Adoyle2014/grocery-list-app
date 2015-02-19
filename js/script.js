97d337b4b6af947cd77f7abe286415b3977c2692
Adam Doyle <dominofoe@gmail.com>
2015-02-18 23:16:37 -0500

    functionality extended

--- js/script.js
+++ js/script.js
@@ -1,49 +1,114 @@
-$(document).ready(function() {
+$(document).ready(function () {
+//watermarks===================================
+    $("#groceryiteminput").watermark("Enter Item");
+    $("#numberpicker").watermark("1gl or 1dz etc.");
 
-	
-	
+//declare global variables================================
+        var catCount = 0;
+        var itemCount = 0;
 
-	$("#submitbutton").click(function() {
-		var newGroceryItem = $("#groceryiteminput").val();
-		var newGroceryItemCat = $("#dropdownlist").val();
-		var newGroceryItemQty = $("#numberpicker").val();
-		var newListItem = '<li class="listitem"><div class="itemquantity">' + newGroceryItemQty + '</div><p class="groceryitem unchecked">' + newGroceryItem + '</p><i class="fa fa-trash-o"></i></li>'
-		var newCatagory = '<ul class="catagory" id="' + newGroceryItemCat + '"><p>' + newGroceryItemCat + '</p>'
-		
+//submit new item action============================
+    $("#submitbutton").click(function () {
 
-		if($("ul#grocerylist").children("#" + newGroceryItemCat).length != 0)
-			switch(newGroceryItemCat) {
-	            		case 'Essentials':
-			    	$("#Essentials").append(newListItem);
-	            	break;
-	            		case 'Dairy':
-			    	$("#Dairy").append(newListItem);
-	            	break;
-	            		case 'Breads':
-			    	$("#Breads").append(newListItem);
-	            	break;
-	            		case 'Produce':
-			    	$("#Produce").append(newListItem);
-	            	break;
-	            	case 'Meats':
-			    	$("#Meats").append(newListItem);
-	            	break;
-	            		case 'Other':
-			    	$("#Other").append(newListItem);
-	            	break;
-	        }
-	        else {
+//declare local variables
+        var newGroceryItem = $("#groceryiteminput").val();
+        var newGroceryItemCat = $("#dropdownlist").val();
+        var newGroceryItemQty = $("#numberpicker").val();
+        var newListItem = '<li class="listitem" data-item-id="' + itemCount + '"><div class="itemquantity">' + newGroceryItemQty + '</div><p class="groceryitem unchecked">' + newGroceryItem + '</p><i class="fa fa-trash-o"></i></li>';
+        var newCatagory = '<ul class="catagory" id="' + newGroceryItemCat + '" data-cat-id="' + catCount + '"><p>' + newGroceryItemCat + '</p>';
+        
+//add items and catagories=======================
+        if ($("#grocerylist").children("#" + newGroceryItemCat).length !== 0) switch (newGroceryItemCat) {
+            case 'Essentials':
+                $("ul#Essentials").append(newListItem);
+                itemCount++;
+                break;
+            case 'Dairy':
+                $("ul#Dairy").append(newListItem);
+                 itemCount++;
+                break;
+            case 'Breads':
+                $("ul#Breads").append(newListItem);
+                 itemCount++;
+                break;
+            case 'Produce':
+                $("ul#Produce").append(newListItem);
+                 itemCount++;
+                break;
+            case 'Meats':
+                $("ul#Meats").append(newListItem);
+                 itemCount++;
+                break;
+            case 'Other':
+                $("ul#Other").append(newListItem);
+                 itemCount++;
+                break;
+        } else {
 
-	        	$("ul#grocerylist").append(newCatagory);
-	        	$( "#" +  newGroceryItemCat).append(newListItem);        	
-	        };
-		
-	});
+            $("ul#grocerylist").append(newCatagory);
+            $("ul#" + newGroceryItemCat).append(newListItem);
+            catCount++;
+            itemCount++;
+        }
+    });
 
-	$('#grocerylist').on('click', '.groceryitem', function() {
-		$(this).toggleClass('unchecked'); 
+//add-remove strike through=======================
+    $('ul#grocerylist').on('click', '.groceryitem', function () {
+        $(this).toggleClass('unchecked');
+    });
+
+ //remove items and catagories=====================
+    $('ul#grocerylist').on('click', 'i.fa-trash-o', function () {
+        var itemId = $(this).closest('li').data('item-id');
+        var catId = $(this).closest('ul').data('cat-id');
+
+        if (($(this).closest('ul.catagory').children('li').length) === 1) {
+            $('ul[data-cat-id="' + catId + '"').remove();
+            
+            console.log(catCount + " - " + itemCount);
+        } else {
+            $('li[data-item-id="' + itemId + '"').remove();
+            itemCount--;
+            console.log(catCount + " - " + itemCount);
+        }
+    });
+
+// views =============================================
+
+// alpha up view ---------------------
+
+    $(".alphaupview").click(function (e) {
+        e.preventDefault();
+        $(".normal, .alphadown").css({
+            display: 'none'
+        });
+        $(".alphaup").css({
+            display: 'inline'
+        });
+    });
+
+// normal view ---------------------
+
+    $(".normalview").click(function (e) {
+        e.preventDefault();
+        $(".alphaup, .alphadown").css({
+            display: 'none'
+        });
+        $(".normal").css({
+            display: 'inline'
+        });
+    });
+
+// alpha down view ---------------------
+
+    $(".alphadownview").click(function (e) {
+        e.preventDefault();
+        $(".normal, .alphaup").css({
+            display: 'none'
+        });
+        $(".alphadown").css({
+            display: 'inline'
+        });
+    });
 });
-	
-	
-	
-});
+
